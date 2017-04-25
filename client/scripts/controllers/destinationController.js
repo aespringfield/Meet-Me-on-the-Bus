4ab7bbd328821dd5e6b590f1f9e58f0dfbfe4a68
Anna Springfield <anna.e.springfield@gmail.com>
2017-04-25 14:29:48 -0500

    clean up

--- client/scripts/controllers/destinationController.js
+++ client/scripts/controllers/destinationController.js
@@ -1,10 +1,4 @@
 myApp.controller('DestinationController', ['UserService', 'moment', function (UserService, moment) {
   let destination = this;
-  destination.now = '';
-  destination.setDate = function() {
-    destination.now = new Date();
-    let momo = moment().add(1, 'week').format('LL');
-    console.log(momo);
-  };
   destination.logout = UserService.logout;
 }]);
--- client/scripts/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-myApp.controller('UserController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
-  $scope.userObject = UserService.userObject;
-  $scope.logout = UserService.logout;
-  let destination = this;
-  destination.printCat = function() {
-    console.log("cat");
-  };
-}]);
