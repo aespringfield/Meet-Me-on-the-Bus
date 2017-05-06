dd00f4fd590324b8fa97e2394ac3fae7aae2e556
Anna Springfield <anna.e.springfield@gmail.com>
2017-05-06 13:30:23 -0500

    add map test

--- client/scripts/controllers/destInputController.js
+++ client/scripts/controllers/destInputController.js
@@ -1,6 +1,13 @@
 myApp.controller('DestInputController', ['$http', '$location', '$mdpTimePicker', 'UserService', 'PersonService', 'moment', function ($http, $location, $mdpTimePicker, UserService, PersonService, moment, $mdpTimePicker) {
   let destInput = this;
 
+  let home = {lat: 44.963376, lng: -93.272385};
+
+  destInput.map = new google.maps.Map(document.getElementById('map'), {
+      center: home,
+      zoom: 15
+    });
+
   let trip = PersonService.userControl.mainUser.currentTrip;
 
   console.log('in dest. trip is', trip);
@@ -22,7 +29,6 @@ myApp.controller('DestInputController', ['$http', '$location', '$mdpTimePicker',
   };
 
   destInput.setInfoAndAdvance = function() {
-    console.log(process.env.SQUIRREL);
     setTripInfo(destInput.searchForm, '/originInput');
   };
 
